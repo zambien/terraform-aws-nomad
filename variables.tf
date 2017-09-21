787f99b931f47585fd00fd4959cde41996c94990
app9n69 <adam_mcneely@troweprice.com>
2017-09-21 18:45:08 +0000

    use nomad beta 0.0.7-beta1

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@
 
 variable "ami_id" {
   description = "The ID of the AMI to run in the cluster. This should be an AMI built from the Packer template under examples/nomad-consul-ami/nomad-consul.json. If no AMI is specified, the template will 'just work' by using the example public AMIs. WARNING! Do not use the example AMIs in a production setting!"
-  default = ""
+  default = "ami-b036dfca"
 }
 
 variable "aws_region" {
@@ -35,12 +35,12 @@ variable "cluster_name" {
 
 variable "num_servers" {
   description = "The number of server nodes to deploy. We strongly recommend using 3 or 5."
-  default     = 3
+  default     = 1
 }
 
 variable "num_clients" {
   description = "The number of client nodes to deploy. You can deploy as many as you need to run your jobs."
-  default     = 6
+  default     = 1
 }
 
 variable "cluster_tag_key" {
